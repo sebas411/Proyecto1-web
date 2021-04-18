42ee48d5e9b59174ca1a61b50c8262bcb6005050
sebas411 <59925024+sebas411@users.noreply.github.com>
2021-04-17 19:17:57 -0600

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>Hola mundo</div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+)
