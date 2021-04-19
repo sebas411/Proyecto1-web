46bb2f2f73db080f17e0fda882c47dc9aa8a587e
sebas411 <59925024+sebas411@users.noreply.github.com>
2021-04-18 23:48:22 -0600

    Actualizaciones

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <button type="button">1</button>
+        </li>
+        <li>
+          <button type="button">2</button>
+        </li>
+        <li>
+          <a href=".">3</a>
+        </li>
+        <li>
+          <button type="button">Sign in</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
+
+export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+
+import './styles.css'
+
 const App = () => (
-  <div>Hola mundo</div>
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
 )
 
 ReactDOM.render(
